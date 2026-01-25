a3d3348740aede2938f64f3220c01027f645c4ec
tony-mm <mwauragabriel800@gmail.com>
2026-01-25 21:09:15 +0300

    initial commit

---
+++ todo.js
@@ -0,0 +1,33 @@
+const input = document.querySelector('#task-input');
+const addBtn = document.querySelector('#add-btn');
+const taskList = document.querySelector('#task-list-ul');
+
+let tasks = [];
+
+addBtn.addEventListener('click', () => {
+    if (input.value === '') return;
+
+    const task = {
+        id: Date.now(),
+        title: input.value,
+        completed: false
+    };
+
+ 
+    tasks.push(task);
+    input.value = '';
+    
+
+    renderTasks(tasks);
+});
+
+function renderTasks(taskArray) {
+    taskList.innerHTML = '';
+
+    taskArray.forEach(task => {
+        const li = document.createElement('li');
+        li.textContent = task.title;
+        taskList.appendChild(li);
+    });
+
+}
